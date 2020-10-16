c0df3959121cba2914db93d46a8d9df154c6df62
chenxin <lightnoway@qq.com>
2020-10-16 11:29:29 +0800

    add page: blog detail

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,27 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: 'http://localhost:1336',
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['post'],
+        // singleTypes: ['impressum'],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: '',
+        //   password: ''
+        // }
+      }
+    }
+  ],
+  templates: {
+    StrapiPost: [{ 
+      path: "/blog/:id",
+      component: './src/templates/Blog.vue'
+    }]
+  }
 }
---
+++ src/templates/Blog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Post</h2>
+    <div>{{$page.blog.title}}</div>
+    <div>{{$page.blog.content}}</div>
+  </div>
+</template>
+<page-query>
+query ($id:ID!){
+  blog: strapiPost(id: $id) { 
+    id
+    title
+    content
+  }
+  # blogs:allStrapiPost{
+  #   edges{
+  #     node{
+  #       title
+  #       content
+  #       id
+  #     }
+  #   }
+  # }
+}
+</page-query>
